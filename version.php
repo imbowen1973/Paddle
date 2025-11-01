8334ec80ae97c225e7d08888e56316673ec64a88
imbowen1973 <mark.bowenvet@gmail.com>
2025-11-01 00:06:26 +0000

    Bump plugin version to 2025103102

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025103100;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2025103102;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100100;        // Requires this Moodle version.
 $plugin->component = 'enrol_paddle';    // Full name of the plugin (used for diagnostics)
